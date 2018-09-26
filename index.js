d7cc56b15398fa856d17cd3ad91d7033deb1c9cd
curiousbabyMz <285532050@qq.com>
2018-09-26 17:32:29 +0800

    office's edit

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
+//works
+var works = {
+    Jumper: {
+        name: '蹦跶菇(wxG)',
+        type: 'miniGame',
+        url: 'https://curiousbabymz.github.io/project/Wechat/MiniGame.jpg'
+    },
+    Production: {
+        name: '产品展示(MiniP)',
+        type: 'miniProgram',
+        url: 'https://curiousbabymz.github.io/project/Wechat/MiniProgram.jpg'
+    },
+    Snake: {
+        name: '贪吃蛇(Ereget)',
+        type: 'miniGame',
+        url: 'https://curiousbabymz.github.io/project/snake/'
+    },
+    PoPo: {
+        name: '泡泡消除(cc)',
+        type: 'miniGame',
+        url: 'https://curiousbabymz.github.io/project/PB2/'
+    },
+    Clock: {
+        name: '3D时钟模型(threejs)',
+        type: 'h5',
+        url: 'https://curiousbabymz.github.io/project/three/'
+    }
+};
+
+//routes
+var routes = [];
+for (let i in works) {
+    routes.push(
+        {
+            path: `/${i}`,
+            component: {
+                template: `
+                <div>
+                    <iframe 
+                    src='${works[i].url}'
+                    height='${window.innerHeight}' 
+                    width='${window.innerWidth}'
+                    ></iframe>
+                </div>
+                `
+            }
+        }
+    )
+}
+
+var router = new VueRouter({
+    routes: routes,
+});
+
+
+
 var app = new Vue({
-    el: '.main',
+    el: '.container',
     data: {
         targetType: '',
+        targetWork: '',
         type: {
             miniProgram: {
                 name: '小程序'
@@ -9,32 +66,19 @@ var app = new Vue({
             miniGame: {
                 name: '小游戏'
             },
-            web: {
-                name: '网页'
+            H5: {
+                name: "H5"
             },
+            // web: {
+            //     name: '网页'
+            // },
         },
-        works: {
-            Jumper: {
-                name: 'Jumper',
-                type: 'miniGame'
-            },
-            Snake: {
-                name: 'Snake',
-                type: 'miniGame'
-            },
-            PoPo: {
-                name: 'PoPo',
-                type: 'miniGame'
-            },
-            Production: {
-                name: 'Production',
-                type: 'miniProgram'
-            },
-        },
+        works: works,
     },
+    router: router,
     methods: {
         nav: function (i) {
-            this.targetType=null;
+            this.targetType = "";
             if (i) this.targetType = i;
             console.log(i)
         }
